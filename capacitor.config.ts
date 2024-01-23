fd2289b0ee8b158fa0ee399af1da1720c66d53df
Vivek9874 <tiwarivivek9874@gmail.com>
2024-01-23 13:13:46 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Lab2Museum',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
